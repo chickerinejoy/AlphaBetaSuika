8dfab2725364901ebcc39a812960e317fc779a8b
ilhye <144883820+ilhye@users.noreply.github.com>
2025-06-08 20:36:23 +0800

    Remove old implementation

--- src/switch.js
+++
@@ -1,8 +0,0 @@
-let currentPlayer = "user";
-
-export const getPlayer = () => currentPlayer;
-
-export const switchPlayer = (player) => {
-    console.log("Switching to:", player);
-    currentPlayer = player;
-}
